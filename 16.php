9450110d5e70b081c703f360becc875a73a9d296
sarvesh1510 <bhardwajsarvesh1510@gmail.com>
2025-01-21 22:57:50 +0530

    Add files via upload

---
+++ 16.php
@@ -0,0 +1,12 @@
+<?php
+
+    $x = "Hello";
+    $y = " World!";
+    echo $x . $y; // Outputs: Hello World!
+    echo "<br>";
+    $x .= $y;
+    echo $x; // Outputs: Hello World!
+    echo "<br>";
+    echo "This Program is written by Sarvesh Bhardwaj 0221BCA062"
+
+?>
